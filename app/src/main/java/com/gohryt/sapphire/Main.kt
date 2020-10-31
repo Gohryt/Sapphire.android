bd6343b9bbabe4f2d98c218ffe7015d5cda76de8
Gohryt <kosachev_alex@mail.ru>
2020-10-31 22:59:44 +0300

    2020.10.31.1

--- app/src/main/java/com/gohryt/sapphire/Main.kt
+++ app/src/main/java/com/gohryt/sapphire/Main.kt
@@ -1,19 +1,15 @@
 package com.gohryt.sapphire
 
-import android.app.Activity
 import android.os.Bundle
 import android.provider.Settings.Secure
-import kotlinx.android.synthetic.main.login.*
+import androidx.appcompat.app.AppCompatActivity
 import libgo.Libgo
 import java.util.*
 
-
-class Main : Activity() {
+class Main : AppCompatActivity() {
     private var session: Boolean = false
     private var properties: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme(R.style.Sapphire)
-        super.onCreate(savedInstanceState)
         Libgo.setProjectVariables(
             applicationInfo.packageName,
             applicationInfo.dataDir,
@@ -26,16 +22,10 @@ class Main : Activity() {
             windowManager.defaultDisplay.height,
             windowManager.defaultDisplay.width
         )
-    }
-
-    override fun onStart() {
-        super.onStart()
         session = Libgo.checkSession()
         properties = Libgo.checkProperties()
-        if (!session) {
-            setContentView(R.layout.login)
-        } else {
-            setContentView(R.layout.main)
-        }
+        setTheme(R.style.Sapphire)
+        setContentView(R.layout.main)
+        super.onCreate(savedInstanceState)
     }
 }
