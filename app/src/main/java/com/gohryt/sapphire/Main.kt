3b6a4aa193668ef8ffbe7801097853177cd4c9d9
Gohryt <kosachev_alex@mail.ru>
2020-11-04 15:19:32 +0300

    2020.11.4.1

--- app/src/main/java/com/gohryt/sapphire/Main.kt
+++ app/src/main/java/com/gohryt/sapphire/Main.kt
@@ -6,31 +6,33 @@ import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.main.*
 import libgo.Libgo
+import libgo.VariablesStruct
 import libgo.PropertiesStruct
 import libgo.SessionStruct
 import java.util.*
 
 class Main : AppCompatActivity() {
+    private lateinit var projectVariables: VariablesStruct
     private lateinit var session: SessionStruct
     private lateinit var properties: PropertiesStruct
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
         splash.visibility = View.VISIBLE
-        Libgo.setProjectVariables(
+        projectVariables = Libgo.setVariables(
             applicationInfo.packageName,
             applicationInfo.dataDir,
             android.os.Build.VERSION.RELEASE,
             android.os.Build.SUPPORTED_ABIS[0],
             android.os.Build.MODEL,
             Locale.getDefault().language,
-            Secure.getString(getContentResolver(), Secure.ANDROID_ID),
+            Secure.getString(contentResolver, Secure.ANDROID_ID),
             android.os.Build.VERSION.SDK_INT,
             windowManager.defaultDisplay.height,
             windowManager.defaultDisplay.width
         )
-        session = Libgo.checkSession()
-        properties = Libgo.checkProperties()
+        session = Libgo.setSession()
+        properties = Libgo.setProperties()
         splash.visibility = View.GONE
     }
 }
