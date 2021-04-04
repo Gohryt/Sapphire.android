a460ad3b1cba972c1b7e0942a94d82955540f5ed
Gohryt <kosachev_alex@mail.ru>
2021-04-04 11:19:36 +0300

    2021.04.1

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -2,10 +2,10 @@ object Versions {
     const val java = "15"
     const val kotlin = "1.4.31"
 
-    const val gradle = "7.0.0-alpha10"
+    const val gradle = "7.0.0-alpha12"
 
-    const val coreKtx = "1.5.0-beta03"
-    const val appCompat = "1.3.0-beta01"
+    const val coreKtx = "1.6.0-alpha01"
+    const val appCompat = "1.3.0-rc01"
 
-    const val compose = "1.0.0-beta02"
+    const val compose = "1.0.0-beta03"
 }
